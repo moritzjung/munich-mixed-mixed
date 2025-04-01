9ca2370ec5a6c2c33724d07bafba90aebc6ae96d
morj <moritz.jung@mpq.mpg.de>
2025-04-01 16:53:32 +0200

    updated start page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title(" :100: Munich Mixed Mixed Games :100: ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "More coming soon!"
 )
